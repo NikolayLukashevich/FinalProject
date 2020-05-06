37f06c7b6863ff9d50181b07b52c9c8ec0fc456a
NikolayLukashevich <61136934+NikolayLukashevich@users.noreply.github.com>
2020-05-06 18:39:56 +0200

    Add some ProductService Test

--- src/test/java/by/lukash/FinalProject/Service/ProductServiceTest.java
+++ src/test/java/by/lukash/FinalProject/Service/ProductServiceTest.java
@@ -4,35 +4,59 @@ import by.lukash.FinalProject.bean.Product;
 
 import by.lukash.FinalProject.repository.ProductRepository;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-
-
 import java.math.BigDecimal;
-import java.util.LinkedList;
-import java.util.List;
+
 
 
 public class ProductServiceTest {
+    ProductService productService = new ProductService();
+    Product product = new Product(
+            "Манго",
+            "Вкусный, сочный",
+            Product.Category.FRUIT,
+            BigDecimal.valueOf(10), //Price
+            BigDecimal.valueOf(0.5),//Discount
+            BigDecimal.valueOf(5)   //actualPrice
+    );
+@Before
+        public void setUp(){
+    ProductRepository.getProduct().clear();
+    ProductRepository.getProduct().add(product);
+}
+
     @Test
     public void getByIdTest() {
-        List<Product> ProductRepo = new LinkedList<>();
+        Product actual = product;
+        Product expected = productService.getById(1L);
+        Assert.assertEquals(expected, actual);
+           }
 
-        Product product = new Product("Манго", "Вкусный, сочный", Product.Category.FRUIT, BigDecimal.valueOf(6.99));
-        Product product1 = new Product("Арбуз", "Большой, красый", Product.Category.BERRIES, BigDecimal.valueOf(2.99));
-        Product product2 = new Product("Огурец", "Длинный, зелёный", Product.Category.VEGETABLE, BigDecimal.valueOf(3.50));
-        ProductRepo.add(product);
-        ProductRepo.add(product1);
-        ProductRepo.add(product2);
-        Long expected1 = 1L;
-        Long actual1 = product.getIdProduct();
-        Assert.assertEquals(expected1, actual1);
-        Long expected2 = 2L;
-        Long actual2 = product1.getIdProduct();
-        Assert.assertEquals(expected2, actual2);
-        Long expected3 = 3L;
-        Long actual3 = product2.getIdProduct();
-        Assert.assertEquals(expected3, actual3);
+    @Test
+    public void add() {
+        Product actual = productService.getById(1L);
+        Product expected = productService.getById(1L);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void addDiscountToProductByNameTest() {
+        productService.addDiscountToProductByName("Манго", BigDecimal.valueOf(0.5));
+        Assert.assertEquals(BigDecimal.valueOf(5.0), product.getActualPrice());
+    }
+//
+//    @Test
+//    void printAll() {
+//
+//    }
+
+    @Test
+    public void removeById() {
+    ProductRepository.getProduct().remove(product);
+    boolean actual = ProductRepository.getProduct().isEmpty();
+    Assert.assertEquals(true, actual);
     }
 }
+
 
